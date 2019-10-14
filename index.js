a003ba74eaff6c34ac54531cf7941d63693c304c
github username <github email address>
2019-10-14 14:47:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^/\s/A-Za-z0-9-']+/g, '')
+  }
+
+  static titleize(string){ 
+     const stringArray = string.split(' ');
+     return stringArray.map((item) => {
+        let prepositionsArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        if ((prepositionsArray.indexOf(item) === -1) ||(stringArray.indexOf(item) === 0)){
+          return item.charAt(0).toUpperCase() + item.slice(1)
+         } else {
+        return item
+          }
+      }).join(' ')
+    }
 }
