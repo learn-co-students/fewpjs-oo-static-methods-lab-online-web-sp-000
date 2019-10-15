277d98df7b9777781be95f2d714dbf7704b47e7d
elijahmm17 <elijahmm17@gmail.com>
2019-10-15 04:22:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(s) {
+    s = s.charAt(0).toUpperCase() + s.slice(1);
+    return s;
+  }
+  
+  static sanitize(s) {
+    return s.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  
+  static titleize(s) {
+    const lowers = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = s.split(' ');
+    words[0] = this.capitalize(words[0]);
+    for (let i = 1; i < words.length; i++) {
+      if (!lowers.find(w => w === words[i])) {
+        words[i] = this.capitalize(words[i]);
+      }
+    }
+    return words.join(' ');
+  }
 }
