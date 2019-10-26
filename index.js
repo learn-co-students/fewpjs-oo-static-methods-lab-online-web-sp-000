da765f1871ac6c6019b463350b14a91cfc7ca2d6
donna <donnacamos88@gmail.com>
2019-10-25 23:04:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.input.replace(/\W/g, ''); 
+  }
+
+  static titleize(str) {  
+    return str.toLowerCase().replace(/(^|\s)\S/g,  
+    (firstLetter) => firstLetter.toUpperCase());
+  }
+  // static titleize(str) {
+  //   let words = str.split(' ');
+  //   for (let n = 0; n < words.length; n++) {
+  //     words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+  //   }
+  //   return words.join(' ');
+  // }
+
 }
