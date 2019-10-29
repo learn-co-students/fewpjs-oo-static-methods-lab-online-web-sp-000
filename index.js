27457a347e0a4d6704b8c9f4a32976777455fe21
abogailo <abagailanne@gmail.com>
2019-10-28 20:35:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+    static capitalize(lower) {
+      return lower.charAt(0).toUpperCase() + lower.substring(1);
+    }
+
+    static sanitize(str) {
+        return str.replace(/[^A-Za-z0-9 '-]/g, "")
+    }
+    
+    static titleize(string) {
+      const notIncluded = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+      const stringArray = string.split(" ").map(function(word, index){
+        if((!notIncluded.includes(word)) || (index === 0)){
+          return Formatter.capitalize(word)
+        } else {
+          return word
+        }
+      })
+
+    return stringArray.join(" ")
+  }
+     
+    
 }
