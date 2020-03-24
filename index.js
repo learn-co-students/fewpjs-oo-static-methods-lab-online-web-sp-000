c33e54e70e3a37d447ea0754a8141a3a2c84b063
Amy Westcott <amy.westcott@yahoo.com>
2020-03-24 15:12:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-\s']+/g, '');
+  }
+
+  static titleize(string){
+      let array = string.split(" ");
+      array.splice(0, 1, this.capitalize(array[0]))
+      for(let i = 1; i < array.length; i++){
+        console.log(array[i])
+        if(!array[i].match(/^(a|the|an|at|but|of|and|by|for|from)$/g)){
+          array.splice(i, 1, this.capitalize(array[i]));
+        }
+      }
+      return array.join(" ")
+  }
+
 }
