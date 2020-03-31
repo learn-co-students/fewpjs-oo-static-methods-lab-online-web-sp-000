3991b0340bda5aeb40bac181c689190863c728e6
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-31 01:15:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let newString = ""
+    let noTitle = ["the", "from", "at", "by", "a", "an", "but", "of", "and", "for"]
+    let title = string.split(" ")
+
+    title.forEach(word => {
+      if (title.indexOf(word) === 0){
+        newString += word.charAt(0).toUpperCase() + word.slice(1)
+      }else if (noTitle.includes(word)){
+        newString += ` ${word}`
+      }else{
+        newString += ` ${word.charAt(0).toUpperCase() + word.slice(1)}`
+      }
+    })
+    return newString
+  }
 }
