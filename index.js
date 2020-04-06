e7511291b8358037d2e6d1ab7bd80334f32f333f
James Lenahan <james.k.lenahan@gmail.com>
2020-04-06 22:00:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.slice(0,1).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]/g, "");
+  }
+
+  static titleize(string){
+    const SKIP_WORDS = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+
+    let titleizedString = string.split(" ").map( x=> {
+      if(SKIP_WORDS.indexOf(x.toLowerCase()) === -1){
+        return Formatter.capitalize(x)
+      } else {
+        return x;
+      }
+    });
+    titleizedString[0] = Formatter.capitalize(titleizedString[0])
+    return titleizedString.join(" ")
+  }
 }
