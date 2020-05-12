a9089e8f9b548a4d254fdc455d530002219dd290
rich0024 <yance0022@gmail.com>
2020-05-12 14:52:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    let newString = string[0].toUpperCase() + string.slice(1)
+    return newString
+  }
+
+  static sanitize(string) {
+    let newString = string.replace(/[^-'\w\s]/g, '')
+    return newString
+  }
+
+  static titleize(string) {
+    let array = string.split(' ')
+
+    const newArray = array.map(x => {
+      if (x === 'the' || x === 'a' || x === 'an' || x === 'but' || x === 'of' || x === 'and' || x === 'for' || x === 'at' || x === 'by' || x === 'from') {
+        return x
+      } else {
+        return (x[0].toUpperCase() + x.slice(1))
+      }
+    })
+
+    let firstWord = newArray[0][0].toUpperCase() + newArray[0].slice(1)
+    newArray.splice(0, 1, firstWord)
+
+    return newArray.join(' ')
+  }
 }
