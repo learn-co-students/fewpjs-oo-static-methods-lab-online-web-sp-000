e695b6f93a91df78df4df3d01736765803b6fba4
zdsmit <zdsmith001@gmail.com>
2020-05-31 08:28:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-zA-Z-' + /i]/g, '')
+  }
+
+  static titleize(string) {
+    let splitString = string.split(" ")
+    let lowerCase = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let result = []
+    for (let i = 0; i < splitString.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(splitString[0]))
+      } else {
+        if (lowerCase.includes(splitString[i])) {
+          result.push(splitString[i])
+        } else {
+          result.push(this.capitalize(splitString[i]))
+        }
+      }
+    }
+
+    return result.join(" ")
+  }
 }
