3c50f772b4dc4c23fd9052956edad58932e4b17c
juan siu <juan.siu@gmail.com>
2020-06-24 01:21:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+
+  static titleize(string){
+
+  }
   //add static methods here
 }
