2829e88810a1108634b7931056be206dee6293ef
juan siu <juan.siu@gmail.com>
2020-06-24 16:13:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ class Formatter {
   }
 
   static sanitize(string){
-    return string.replace(/[^A-Za-z0-9-']+/g, '');
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
 
   static titleize(string){
+    let stringArray = string.split(" ");
+    let newArray = [];
+    for (let i = 0; i < stringArray.length; i++){
+        if (stringArray[i] === "a" || stringArray[i] === "the" || stringArray[i] === "an" || stringArray[i] === "but" || stringArray[i] === "of" || stringArray[i] === "and" || stringArray[i] === "for" || stringArray[i] === "at" || stringArray[i] === "by" || stringArray[i] === "from"){
 
+          if (i === 0 ){
+            newArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
+          } else {
+            newArray[i] = stringArray[i];
+          }
+
+      } else {
+        console.log("capitalize this", stringArray[i])
+        newArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
+      }
+    }
+    return newArray.join(" ");
   }
   //add static methods here
 }
