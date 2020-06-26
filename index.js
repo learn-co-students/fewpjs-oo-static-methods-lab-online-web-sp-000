e53f36cf0075cde3128722cc9af8cabc04fa2dea
nathan-stamand <nathansaint810@gmail.com>
2020-06-26 13:53:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let title = string.split(' ').map(word => {
+      if (!exceptions.find((exception) => exception === word) || word === string.split(' ')[0]) {
+        return Formatter.capitalize(Formatter.sanitize(word))
+      }
+      else {
+        return Formatter.sanitize(word)
+      }
+    })
+    return title.join(' ')
+  }
 }
