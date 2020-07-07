082c1564768873c675bb169c5b3b1276e3d70099
Dan-zai <danielcowpertwait@gmail.com>
2020-07-07 15:32:27 +1000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    const exceptions = ['the','a','an','but','of','and','for','at','by','from']
+    let newString = string.charAt(0).toUpperCase() + string.slice(1)
+    let stringArray = newString.split(" ")
+    return stringArray.map(string => {
+      if (exceptions.includes(string)) {
+        return string
+      } else {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      }
+    }).join(" ")
+  }
+
 }
