53bc4dd4c368a2907c19820aadd97db6bc01741d
dayofgray <graysonpberger@gmail.com>
2020-07-11 15:07:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let array = string.split(" ")
+    let newArray = []
+    newArray.push(Formatter.capitalize(array[0]))
+    for (let i = 1; i < array.length; i++) {
+      if (exceptions.includes(array[i]) ) {
+        newArray.push(array[i])
+      }
+      else {
+        newArray.push(Formatter.capitalize(array[i]))
+      }
+    }
+    return newArray.join(" ")
+  }
 }
