0453dc2b49700b1a2a039807246e172c5f0a9050
Anthony Tilelli <Anthony@tilelli.me>
2020-07-30 00:20:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return `${str[0].toUpperCase()}${str.slice(1)}`;
+  }
+  static sanitize( str ) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+  static titleize(title){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const titleArr = title.split(' ');
+    const modified = titleArr.slice(1).map(word => (exceptions.some(element => element === word)) ? word : this.capitalize(word));
+    modified.unshift(this.capitalize(titleArr[0])); // Add back first word
+    return modified.join(" ");
+  }
 }
