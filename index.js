4e4e98d11c4b373d8729c438f41c37a97b241311
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-01 18:45:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^0-9a-zA-Z '-]/g, '')
+  }
+
+  static titleize() {
+
+  }
+
 }
