eeb3577c32c5a38bbb6f3248b57ec8364e9f6636
itsbino <charles.sabino@hotmail.com>
2020-08-07 00:35:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string) {
+    return string.replace(/[^-,', A-Za-z0-9]+/g, '')
+  }
+
+  static titleize (string) {
+    const words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = string.toLowerCase().split(' ').map( words => {
+      if (words === 'the' || words === 'a' || words === 'an' || words === 'but' || words === 'of' || words === 'and' || words === 'for' || words === 'at' || words === 'by' || words === 'from') {
+        return words
+      } else {
+        return words[0].toUpperCase() + words.slice(1) 
+      }
+    }).join(' ')
+    return newString[0].toUpperCase() + newString.slice(1)
+    }
+  }
