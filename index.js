bb1593292a8502eca294f05fea2068f06b54a1f1
Chris Kay <ckay84@protonmail.com>
2020-08-27 14:56:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let first = string.charAt(0).toUpperCase()
+    return first + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let words = string.split(' ')
+    let wordArray = words.map(word => {
+      if (word === words[0]) {
+        return this.capitalize(word)
+      } else if (exceptions.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    })
+    return wordArray.join(' ')
+  }
 }
