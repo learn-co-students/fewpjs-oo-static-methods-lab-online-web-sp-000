e0dec5838ca65bd100103d2b9ae899cc4ef6e390
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-09 14:51:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'" "]+/g, '');
+  }
+
+  static titleize(string) {
+    let lowercase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let capString = string.charAt(0).toUpperCase() + string.slice(1);
+    let arrayOfWords = capString.split(" ");
+    let newArrayOfWords = arrayOfWords.map(function(word) {
+      if (lowercase.indexOf(word) === -1) {
+        return word.charAt(0).toUpperCase() + word.slice(1); 
+      } else {
+        return word
+      }
+    })
+    return newArrayOfWords.join(" ");
+  }
+
 }
