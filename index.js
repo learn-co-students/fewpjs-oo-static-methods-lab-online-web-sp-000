809a66db1cf9a5ea36289aea2e487356cea3de13
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-09-20 15:00:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+	static capitalize(str) {
+		return str[0].toUpperCase() + str.slice(1)
+	}
+
+	static sanitize(str) {
+		return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+	}
+
+	static titleize(str) {
+		const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+		let words = this.capitalize(str).split(" ")
+		return words.map(word => exceptions.includes(word) ? word : this.capitalize(word)).join(" ")
+	}
 }
