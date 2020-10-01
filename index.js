70e0e27c80d7c5d679a10278fa3830be38b2e2b2
Chance Smith <chancesmith97@hotmail.com>
2020-10-01 01:56:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[&\/\\#,+()$~%.":*?<>^{}!@]/g, '');
+  }
+
+  static titleize(string){
+    const except = ["of", "the", "a", "an", "and", "but", "for", "at", "by", "from"]
+
+    return string.split(" ").map(function(word, index) {
+      if (index !== 0 && except.includes(word)) {
+            return word
+          } else {
+            return this.capitalize(word)
+          }
+        }.bind(this)).join(" ")
+    }
 }
