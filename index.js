592073240ff2e5228fe23ff3f2d2c5491ee3f560
Indre Bakeer <indre.bakeer@gmail.com>
2020-10-23 00:35:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let array = this.capitalize(string).split(" ");
+    return array.map(word => {
+      if (exceptions.includes(word)) {
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    }).join(" ");
+  }
+
 }
