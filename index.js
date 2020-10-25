587806634f47ac99f3eed60430b684f44acd9991
carrollm2 <carrollm2@gmail.com>
2020-10-24 21:11:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(givenString){
+    return givenString.charAt(0).toUpperCase() + givenString.slice(1)
+  }
+
+  static sanitize(givenString){
+    const sanitizedString = givenString.replace(/[^\w\s,'-]/gi, '')
+    return sanitizedString
+  }
+
+  static titleize(givenString){
+
+    function titleize(givenString) {
+      if(!givenString.split) return givenString;
+      var _titleizeWord = function(string) {
+              return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+          },
+          result = [];
+      givenString.split(" ").forEach(function(w) {
+          result.push(_titleizeWord(w));
+      });
+      return result.join(" ");
+  }    
+
+  }
 }
