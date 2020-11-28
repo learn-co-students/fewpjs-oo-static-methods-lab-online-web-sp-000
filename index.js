960455a2743c0bb9173eb0bab2407363fa5ebd0f
The-Gentleman <zraja4402@gmail.com>
2020-11-28 15:21:29 -0500

    Done.

--- index.js
+++ index.js
@@ -10,18 +10,23 @@ class Formatter {
   static titleize(string){
     // split the string
     let word = string.split(' ');
+    // console.log(word)
     // put all the lowercase letters in an array
     const lowercaseArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
     // iterate through the string
     // capitalize the first word and the first letter of each word
     // except the lowercase ones.
+    let resultArray = [];
     for (let i = 0; i < word.length; i++){
-      for(let j = 1; j < lowercaseArray.length; j++){
-        if (i = 0 && word[i] !== lowercaseArray[j]){
-          word[i].toUpperCase()
-        }
+      // this checks if the letter is lowercase
+       if (i == 0) {
+        resultArray.push(this.capitalize(word[i]));
+      } else if ((lowercaseArray.includes(word[i]))){
+        resultArray.push(word[i]);
+      } else {
+        resultArray.push(this.capitalize(word[i]));
       }
     }
-      return word.join(' ');
+      return resultArray.join(' ');
   }
 }
