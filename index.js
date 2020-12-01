fbc32dbb4e1a8b1a0f8b33bfb3e73adcd087c305
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-01 04:15:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ class Formatter {
   //add static methods here
 //Write a method static capitalize that takes in a string
 //and capitalizes the first letter.
-static capitalize(string){
-  return string.charAt(0).toUpperCase + string.ice(1)
+static capitalize( string ) {
+   return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+ }
+//Write a method static sanitize that takes in a string and
+//removes all non-alphanumeric characters except for dashes,
+//single quotes and spaces.
+static sanitize(string){
+  
 }
-//Write a method static sanitize that takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces.
 //Write a method static titleize that takes in a string and capitalizes all words in a sentence except
 //the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word.
 }
