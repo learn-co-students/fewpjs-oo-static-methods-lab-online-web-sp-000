0a1a419d4f805017fc2b15533393c72655620fd1
CoderJay06 <jaycru06@yahoo.com>
2021-01-14 23:36:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+    //add static methods here
+
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9- ']+/g, '');
+    }
+
+    static titleize(string) {
+        // 1) is a static method
+        // 2) capitalizes all words in a sentence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'
+        // 3) always capitalizes the first word
+        string
+    }
+
+    _isNotExcepted(word) {
+        switch (word) {
+            case 'the':
+            case 'a':
+            case 'an':
+            case 'but':
+            case 'for':
+            case 'at':
+            case 'by':
+            case 'from':
+                return true;
+            default:
+                return false;
+        }
+    }
 }
