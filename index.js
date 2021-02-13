2243861bb1aa6865fdfdbd3e71233ff321e1b164
William Pannell <lpann5@gmail.com>
2021-02-12 21:55:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(string){
+    let badword = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let answer = []
+    let words = string.split(" ")
+    for (let i = 0; i < words.length; i++)
+    {
+      if (i == 0)
+      {
+        answer.push(this.capitalize(words[i]))
+      }
+      else if (badword.includes(words[i]))
+      {
+        answer.push(words[i])
+      }
+      else
+      {
+        answer.push(this.capitalize(words[i]))
+      }
+    }
+    return answer.join(" ");
+  }
 }
