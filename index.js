8f5a38d30df65d30f07c8cf710e1309c6f88bb35
Catherine Gloria <catherinegloria@gmail.com>
2021-02-16 13:42:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string){
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let array = string.split(" ")
+    let newArr = [];
+
+    for(let i = 0; i < array.length; i++){
+      if (exceptions.find(e => e === array[i]) && array[i] !== array[0] ) {
+        newArr.push(array[i])
+      } else {
+        newArr.push(this.capitalize(array[i]))
+      }
+    }
+
+    return newArr.join(" ")
+  }
+
 }
