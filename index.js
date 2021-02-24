1de49357a11edb32f5c9c341ba2a84616d0298e1
jrog20 <jaimerogstad@gmail.com>
2021-02-24 18:26:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-\s-']+/g, '');
+  }
+
+  static titleize(string) {
+    return string.split(" ").map(function(word, index){
+    if (!word.match(/^the$|^[a]$|an|but|of|and|for|at|by|from/) || index == 0){
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    } else {
+      return word;
+    }}).join(" ")
+  }
 }
