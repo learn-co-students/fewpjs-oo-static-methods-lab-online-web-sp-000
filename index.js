677c66fe34f22b454cf52c32a01352ac98a9cc88
john million <jpmillion@comcast.net>
2021-02-25 21:36:49 -0500

    Done.

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ class Formatter {
 
   static titleize(string) {
     const words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
-    string = string.split();
+    string = string.split(' ');
+    const result = [];
     for (let i = 0; i < string.length; i++) {
       if (i === 0) {
-        this.capitalize(string[i]);
-      } else if (!words.some((word) => word === string[i])) {
-        this.capitalize(string[i]);
+        result.push(this.capitalize(string[i]));
+      } else if (words.includes(string[i])) {
+        result.push(string[i]);
+      } else {
+        result.push(this.capitalize(string[i]));
       }
     }
-    return string.join(' ');
+    return result.join(' ');
   }
 
   static sanitize(string) {
-    string = string.replace(/[^a-z0-9]/i, "");
-    return string;
+    return string.replace( /[^a-z0-9 '-]/gi, '' )
   }
 }
