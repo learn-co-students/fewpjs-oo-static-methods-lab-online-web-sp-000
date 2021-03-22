16b94353888b3a4472b526e2d20c8468c05835a9
Carrie Mathieu <carrieburks@gmail.com>
2021-03-22 00:18:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return ((str[0].toUpperCase()) + str.slice(1))
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'" "]+/g, '');
+  }
+
+  static titleize(str) {
+    let forbidden = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let newsent = []
+    let caps = str.split(" ")
+    for (let i=0; i < caps.length; i++) {
+      if (!forbidden.includes(caps[i])) {
+        let word = caps[i][0].toUpperCase() + caps[i].slice(1);
+        newsent.push(word)
+      } else if (i == 0) {
+        let word = caps[i][0].toUpperCase() + caps[i].slice(1);
+        newsent.push(word)
+      } else {
+        newsent.push(caps[i])
+      }
+    }
+    return newsent.join(" ")
+  }
 }
