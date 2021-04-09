795431167af8fd3abdce9cd91e95be68c4266743
jeanettepayne <jeanetterpayne@gmail.com>
2021-04-08 20:45:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+
+  static sanitize(input) {
+    return input.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(input) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let title = [];
+    let wordArray = input.split(' ');
+
+    for (let n = 0; n < wordArray.length; n++) {
+      if (n == 0) {
+        title.push(this.capitalize(wordArray[n]))
+      } else if (exceptions.includes(wordArray[n])) {
+        title.push(wordArray[n])
+      } else {
+        title.push(this.capitalize(wordArray[n]))
+      }
+    }
+    return title.join(' ');
+  }
 }
