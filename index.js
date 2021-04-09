b7bd1c1f1cd53058528ccec2bbedc1ea5e3d3088
Arodriguez1030 <arodriguez1030@gmail.com>
2021-04-09 18:28:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  }
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let wordArray = string.split(" ")
+    for (let n = 0; n< wordArray.length; n++){
+      if(n == 0) {
+        result.push( this.capitalize( wordArray[n] ) )
+      }
+      else {
+        if (exceptions.includes( wordArray[n] ) ) {
+          result.push( wordArray[n] )
+        }
+        else {
+          result.push( this.capitalize( wordArray[n] ) )
+        }
+      }
+      
+    }
+    return result.join( " " );
+  }
 }
