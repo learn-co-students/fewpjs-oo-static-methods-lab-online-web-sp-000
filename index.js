244cc582bf30a3d725234d8ba29ba297e5abd959
sbigelo <sean.bigelow1@gmail.com>
2021-05-11 15:40:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9 '-]+/g, '');
+    }
+
+    static titleize(string) {
+      let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      let results = []
+      let arrayOfWords = string.split(' ')
+      for (let i = 0; i < arrayOfWords.length; i++) {
+        if (i === 0) {
+          results.push(this.capitalize(arrayOfWords[i]))
+        } else {
+          if (exceptions.includes(arrayOfWords[i])) {
+            results.push(arrayOfWords[i])
+          } else {
+            results.push(this.capitalize(arrayOfWords[i]))
+          }
+        }
+      }
+      return results.join(' ')
+    }
 }
