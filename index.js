4441e852616851df8da008f9475e7e4ea9f2a779
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-31 16:02:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.'-]/gim, "");
+    return str.trim();
+  }
+
+  static titleize(str) {
+    let words = str.split(' ')
+    const ignore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = words.map(word => {
+      if (!ignore.some(e => e === word)) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    })
+    let final = Formatter.capitalize(arr.join(' '))
+    return final
+  }
 }
