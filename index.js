4d0556995c236d39edd05ab2de6e7c5632c619f9
bongofury <jtroutmanman@gmail.com>
2021-05-30 21:14:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize(string) {
+    let result = [];
+    let except = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let stringArray = string.split(" ");
+    result.push(this.capitalize(stringArray[0]));
+    stringArray.slice(1).forEach((x) => {
+      if (except.includes(x)) {
+        result.push(x);
+      } else {
+        result.push(this.capitalize(x));
+      }
+    });
+    return result.join(" ");
+  }
 }
